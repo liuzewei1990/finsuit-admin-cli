f67616aa827aa184b57c7bc50685c1db0add2d77
刘泽伟 <liuzewei@bicai365.com>
2020-01-09 18:14:32 +0800

    第一次提交

---
+++ README.md
@@ -0,0 +1,87 @@
+## 比财H5活动开发架构
+[TOC]
+> vue
+
+### npm命令说明
+
+``` bash
+# install dependencies
+npm install
+
+# 启动开发环境 代表启动全部微项目 并设置开发环境配置
+npm run dev
+# 启动开发环境 代表启动指定微项目 并设置开发环境配置
+npm run dev -project1 -project2 -project3 -project4......
+
+# 发布测试包 打包指定的微项目 并设置测试环境配置
+npm run build:test [项目名称必填]
+
+# 发布生产包 打包指定的微项目 并设置生产环境配置
+npm run build:prod [项目名称必填]
+
+# 查看打包报告
+npm run build:xxx [xxx] --report
+```
+
+### 微前端架构特点
+- **项目规范 - 约束规范** ：多个微项目应用一套脚手架，一套公共库，一套公共模块，统一项目规范。
+- **开发阶段 - 独立开发** ：一键启动所有微项目开发环境，支持微项目之间间接联调。独立开发所属项目包，完全解决代码冲突问题，公共模块分离；
+- **发布阶段 - 独立发布** ：支持测试环境和生产环境发布，走不通配置，微项目单独打包，单独生成dist包；
+- **部署阶段 - 独立部署** ：支持一键部署所有微项目生产包，支持独立项目部署，利用浏览器缓存机制，优化公共库的缓存。
+
+### 源码目录结构
+``` bash
+├─common
+│  ├─finsuit-assets --------------------所有微项目的公共资源
+│  │  ├─icomoon
+│  │  │  ├─demo-files
+│  │  │  └─fonts
+│  │  ├─images
+│  │  └─less
+│  ├─finsuit-components ----------------所有微项目的公共组件
+│  │  ├─Button
+│  │  │  └─less
+│  │  ├─Confirm
+│  │  ├─EllipsisText
+│  │  ├─HttpErrorDialog
+│  │  ├─Loading
+│  │  ├─MaskBox
+│  │  ├─Popup
+│  │  └─Toast
+│  ├─finsuit-h5 ------------------------所有微项目的比财H5开发插件包
+│  │  └─libs
+│  ├─finsuit-http ----------------------所有微项目的请求封装处理
+│  └─finsuit-store ---------------------所有微项目的H5开发插件包辅助store
+└─pages
+    └─templateA ------------------------微项目A
+    │ ├─apis
+    │ ├─config
+    │ ├─router
+    │ ├─store
+    │ └─view
+    │      └─main
+    │          └─modules
+    └─templateB ------------------------微项目B
+    │ ├─apis
+    │ ├─config
+    │ ├─router
+    │ ├─store
+    │ └─view
+    │     └─main
+    │         └─modules
+```
+### 打包目录结构
+``` bash
+├─templateA -------------------------- 微项目A dist包
+│  └─static
+│      ├─css
+│      ├─fonts
+│      ├─img
+│      └─js
+└─templateB -------------------------- 微项目B dist包
+    └─static
+        ├─css
+        ├─fonts
+        └─js
+```
+-------------------
---
+++ build/webpack.dev.conf.js
@@ -0,0 +1,113 @@
+'use strict'
+const utils = require('./utils')
+const webpack = require('webpack')
+const config = require('../config')
+const merge = require('webpack-merge')
+const path = require('path')
+const baseWebpackConfig = require('./webpack.base.conf')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
+const portfinder = require('portfinder')
+
+const HOST = process.env.HOST
+const PORT = process.env.PORT && Number(process.env.PORT)
+let entrys = utils.entries();
+
+let cooked = JSON.parse(process.env.npm_config_argv).cooked;
+cooked = cooked.slice(2);
+if (cooked.length > 0) {
+    let newEntrys = {};
+    for (let index = 0; index < cooked.length; index++) {
+        const devPackName = cooked[index].replace(/-/, "");
+        newEntrys[devPackName] = entrys[devPackName];
+    }
+    entrys = newEntrys;
+}
+
+
+
+const devWebpackConfig = merge(baseWebpackConfig, {
+
+    entry: entrys,
+    module: {
+        rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap, usePostCSS: true })
+    },
+    // cheap-module-eval-source-map is faster for development
+    devtool: config.dev.devtool,
+
+    // these devServer options should be customized in /config/index.js
+    devServer: {
+        // clientLogLevel: 'warning',
+        // index: path.join(config.dev.assetsPublicPath, 'template.html'),
+        // historyApiFallback: {
+        //     rewrites: [
+        //         { from: /.*/, to: path.posix.join(config.dev.assetsPublicPath, 'index.html') },
+        //     ],
+        // },
+        // hot: true,
+        // contentBase: path.join(__dirname, "main"), // since we use CopyWebpackPlugin.
+        // compress: true,
+        // host: HOST || config.dev.host,
+        // port: PORT || config.dev.port,
+        // open: config.dev.autoOpenBrowser,
+        // overlay: config.dev.errorOverlay
+        //     ? { warnings: false, errors: true }
+        //     : false,
+        // publicPath: config.dev.assetsPublicPath,
+        // proxy: config.dev.proxyTable,
+        // quiet: true, // necessary for FriendlyErrorsPlugin
+        // watchOptions: {
+        //     poll: config.dev.poll,
+        // }
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': require('../config/dev.env')
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin(), // HMR shows correct file names in console on update.
+        new webpack.NoEmitOnErrorsPlugin(),
+        // https://github.com/ampedandwired/html-webpack-plugin
+        // new HtmlWebpackPlugin({
+        //     filename: 'index.html',
+        //     template: 'index.html',
+        //     inject: true
+        // }),
+        // copy custom static assets
+        new CopyWebpackPlugin([
+            {
+                from: path.resolve(__dirname, '../static'),
+                to: config.dev.assetsSubDirectory,
+                ignore: ['.*']
+            }
+        ])
+    ].concat(utils.htmlPlugin(Object.keys(entrys)))
+})
+
+
+module.exports = new Promise((resolve, reject) => {
+    portfinder.basePort = process.env.PORT || config.dev.port
+    portfinder.getPort((err, port) => {
+        if (err) {
+            reject(err)
+        } else {
+            // publish the new Port, necessary for e2e tests
+            process.env.PORT = port
+            // add port to devServer config
+            devWebpackConfig.devServer.port = port
+
+            // Add FriendlyErrorsPlugin
+            devWebpackConfig.plugins.push(new FriendlyErrorsPlugin({
+                compilationSuccessInfo: {
+                    messages: [`开发环境全部已启动 在浏览器中输入对应的项目文件预览吧 例如: http://${devWebpackConfig.devServer.host}:${port}/template.html`],
+                },
+                onErrors: config.dev.notifyOnErrors
+                    ? utils.createNotifierCallback()
+                    : undefined
+            }))
+
+            resolve(devWebpackConfig)
+        }
+    })
+})
